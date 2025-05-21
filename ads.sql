d15851cf24c7c30020931ba28fb405d316dbcaf1
Ekram-ekiti <ekru6482@gmail.com>
2025-05-21 16:59:39 +0300

    other pages added

--- ads.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE ads (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    image_url VARCHAR(255),  -- URL to the ad's image
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- When the ad was created
-    user_id INT, -- Foreign key linking to the user table 
-    FOREIGN KEY (user_id) REFERENCES user(id) 
-);
